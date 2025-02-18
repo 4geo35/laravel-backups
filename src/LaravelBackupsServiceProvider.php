e8b7833fdbef1e02198584c4366e98dcbe99892d
Labrock <vladimirpeshkov5@yandex.ru>
2025-02-18 15:33:04 +0300

    extend storage

--- src/LaravelBackupsServiceProvider.php
+++ src/LaravelBackupsServiceProvider.php
@@ -2,14 +2,21 @@
 
 namespace GIS\LaravelBackups;
 
+use Aws\Sdk;
 use GIS\LaravelBackups\Console\Commands\BackupApplicationCommand;
 use GIS\LaravelBackups\Console\Commands\BackupDataBaseCommand;
 use GIS\LaravelBackups\Console\Commands\BackupStorageCommand;
+use GIS\LaravelBackups\Console\Commands\PullApplicationCommand;
+use GIS\LaravelBackups\Console\Commands\PushApplicationCommand;
 use GIS\LaravelBackups\Console\Commands\RestoreApplicationCommand;
 use GIS\LaravelBackups\Console\Commands\RestoreDataBaseCommand;
 use GIS\LaravelBackups\Console\Commands\RestoreStorageCommand;
 use GIS\LaravelBackups\Helpers\ZipActionsManager;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
+use League\Flysystem\Filesystem;
 
 class LaravelBackupsServiceProvider extends ServiceProvider
 {
@@ -24,6 +31,9 @@ class LaravelBackupsServiceProvider extends ServiceProvider
 
             BackupApplicationCommand::class,
             RestoreApplicationCommand::class,
+
+            PushApplicationCommand::class,
+            PullApplicationCommand::class,
         ]);
         // Export config
         $this->publishes([
@@ -42,6 +52,7 @@ class LaravelBackupsServiceProvider extends ServiceProvider
             'region' => config("laravel-backups.region"),
             'bucket' => config("laravel-backups.bucket"),
         ];
+        $this->extendStorage();
     }
 
     public function register(): void
@@ -57,5 +68,31 @@ class LaravelBackupsServiceProvider extends ServiceProvider
         // Routes
         $this->loadRoutesFrom(__DIR__ . "/routes/api.php");
         $this->loadRoutesFrom(__DIR__ . "/routes/admin.php");
+    }
+
+    protected function extendStorage(): void
+    {
+        Storage::extend("yaS3Backups", function ($app, $config) {
+            $configS3 = [
+                "endpoint" => "https://storage.yandexcloud.net",
+                "region" => $config['region'],
+                "version" => "latest",
+                "credentials" => [
+                    "key" => $config["key"],
+                    "secret" => $config["secret"],
+                ],
+            ];
+            if (config("app.debug")) {
+                $configS3['http'] = [
+                    'verify' => false
+                ];
+            }
+            $sdk = new Sdk($configS3);
+            $s3 = $sdk->createS3();
+
+            $adapter = new AwsS3V3Adapter($s3, $config["bucket"]);
+
+            return new FilesystemAdapter(new Filesystem($adapter, $config), $adapter, $config);
+        });
     }
 }
